fa156793b23d1ee8e75d49006606bf7881c4382e
Ryan Campbell <Rpcampbell434@aol.com>
2020-05-29 11:42:24 -0400

    Added Hooks file, did not connect yet

---
+++ src/hooks/hooks.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export const useAppState = () => {
+  const [count, setCount] = useState(0);
+  const [deck, setDeck] = useState([]);
+  const [runningCount, setRunningCount] = useState(0);
+  const [userAnswer, setUserAnswer] = useState(0);
+  const [chosenCard, setChosenCard] = useState({
+    suit: null,
+    face: "0",
+    image: null,
+  });
+  const [answerMode, setAnswerMode] = useState({
+    checkAnswerMode: false,
+    correctAnswer: false,
+  });
+  const [gameMode, setGameMode] = useState({
+    numberOfDecks: 1,
+    dealerMode: "click",
+  });
+  return [
+    count,
+    setCount,
+    deck,
+    setDeck,
+    runningCount,
+    setRunningCount,
+    userAnswer,
+    setUserAnswer,
+    chosenCard,
+    setChosenCard,
+    answerMode,
+    setAnswerMode,
+    gameMode,
+    setGameMode,
+  ];
+};
