472dd99d44087a4ad64a11b140b7cb11dc471507
Ryan Campbell <Rpcampbell434@aol.com>
2020-06-05 13:16:09 -0400

    Adding connection from API to Client apps

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { CardCountingApp } from "./Components/CardCountingApp";
 
 const App = () => {
+  const [state, setState] = useState({ apiResponse: "" });
+
+  function callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setState({ apiResponse: res }));
+  }
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
   return (
     <div>
+      <p>{state.apiResponse}</p>
       <CardCountingApp />
     </div>
   );
