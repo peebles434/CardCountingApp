75b268396cdeb3a140c9316dfdf6d8a6772d6966
Ryan Campbell <Rpcampbell434@aol.com>
2020-06-20 09:37:28 -0400

    Setup autoStopper, need to setup reset btn and autostopper at end of dek

--- src/Components/CardBack.js
+++ src/Components/CardBack.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import { back } from "../utils/cardPics";
 import { useStore } from "../Stores/rootStore";
-import { timerDuration } from "../Logic/CardBackLogic";
+import { timerDuration, randomDealerStop } from "../Logic/CardBackLogic";
 
 export const CardBack = observer(({ drawFromDeck, count, deck }) => {
+  // Imported dealermode info from mobX
   const { viewDealerMode, viewDealerDifficulty } = useStore();
 
+  // States used by autodealer interval
   const [isActive, setIsActive] = useState(false);
   const [autoCount, setAutoCount] = useState(0);
+  const [dealerStopPoint, setDealerStopPoint] = useState(20);
 
   const clickHandler = () => {
+    // If click is to turn on timer, it draws 1st card instantly instead of waiting 1-2 seconds
     if (!isActive) {
       drawFromDeck();
+      setAutoCount(1);
     }
+    // Turns on timed auto-dealer
     setIsActive(!isActive);
   };
 
+  // AutoDealing Function - uses timerDuration in CardBackLogic.js to set interval speed
   useEffect(() => {
     let interval = null;
     if (isActive) {
@@ -29,6 +36,19 @@ export const CardBack = observer(({ drawFromDeck, count, deck }) => {
     }
     return () => clearInterval(interval);
   }, [isActive, autoCount, drawFromDeck]);
+
+  // TODO: Should Stop autodealer randomly based on CardBackLogic.js (10-40 by default)
+  // TODO: Need to hook up to randomDealerStop from Logic folder
+  useEffect(() => {
+    setDealerStopPoint(randomDealerStop());
+  }, []);
+
+  useEffect(() => {
+    console.log(autoCount + " - " + dealerStopPoint);
+    if (autoCount === dealerStopPoint) {
+      setIsActive(!isActive);
+    }
+  }, [autoCount]);
 
   return (
     <div>
--- src/Logic/CardBackLogic.js
+++ src/Logic/CardBackLogic.js
@@ -1,3 +1,4 @@
+// Sets interval duration based on mobX's dealer mode
 export const timerDuration = (mode) => {
   if (mode === "easy") {
     return 2000;
@@ -6,4 +7,10 @@ export const timerDuration = (mode) => {
   } else if (mode === "hard") {
     return 1000;
   }
+};
+
+// Creates a random number between 20 and 40;
+export const randomDealerStop = () => {
+  let random = Math.floor(Math.random() * 30);
+  return random + 10;
 };
